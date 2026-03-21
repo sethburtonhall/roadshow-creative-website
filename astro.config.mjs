8865868de035a4fc95e6b02d5f87a18e451321f0
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2026-03-20 17:04:41 -0700

    Vercel best practices and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercel(),
   site: "https://roadshowcreative.dev",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
